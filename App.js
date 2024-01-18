0e8271533f6dd04757cbb6bcf1f914c0f8533dba
unusG <uniclusgao@gmail.com>
2024-01-17 16:06:02 -0800

    Add a new component Input

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Header from './components/Header';
+import Input from './components/Input';
 
 export default function App() {
   const appName = "My awesome app"
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
+      <Header name={appName} version={2} />
+      <Input />
+      {/* <Text>{text}</Text> */}
     </View>
   );
 }
@@ -18,4 +22,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
